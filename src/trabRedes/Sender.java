3c1bbf6461c394617f7999c76cfc3be8986ba70e
unknown <13205014@W1846690.portoalegre.pucrsnet.br>
2015-05-25 19:44:13 -0300

    pronto

--- src/trabRedes/Sender.java
+++ src/trabRedes/Sender.java
@@ -33,8 +33,8 @@ public class Sender implements Runnable {
         while (true) {            
             synchronized(table) {
                 if(!table.isEmpty()) {
-                    System.out.println("enviando tabela:");
-                    System.out.println(table);
+                    //System.out.println("enviando tabela:");
+                    //System.out.println(table);
                     for (RouteLine line : table) {
                         if(line.getMetric() == 1)  {
                             InetAddress saida = table.getSaida(line);
